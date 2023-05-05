3ee5b4e10b6ba6f3be4119d2b1c1edf1050ed136
Matheus Reis Fonseca <matheuzim011@hotmail.com>
2023-05-05 15:05:34 -0300

    Estilizado Default Button Component

--- src/components/defaultButton/styles.tsx
+++ src/components/defaultButton/styles.tsx
@@ -8,13 +8,21 @@ type ContenerProps = {
 export const Contener = styled.button<ContenerProps>`
     height : ${({ h }) => (h ?? "100%")};
     width: 100%;
-    background-color: red;
+    background-color:#6A5ACD;
+    border-radius: 5px;
+    align-items: center;
+    display: flex;
+    justify-content: space-evenly;
+    border: 1px solid #6A5ACD;
     `
 
 export const Text = styled.span`
-
+    color: white;
+    font-size: 15px;
+    font-weight: bold;
     `
 
 export const Icon = styled(FontAwesomeIcon)`
-    font-size:  20px;
-`
+    font-size:  15px;
+    color: white;
+    `
