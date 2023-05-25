2c20dc0f5f967eb449d91684057e87b9229f4149
Matheus Reis Fonseca <matheuzim011@hotmail.com>
2023-05-25 17:58:58 -0300

    Middleware funcionando

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import validateToken from "./src/util/validateCookie";
+import validateToken from "./src/util/validateToken";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req:NextRequest) {
@@ -8,18 +8,17 @@ export async function middleware(req:NextRequest) {
         return
     }
 
-    const token = req.cookies.get('AccessToken')?.value ?? ''
-    console.log('altar', token)
+    const token = req.cookies.get('AccessToken')?.valueOf() ?? ''
 
-    if(pathname.startsWith('/auth/login') && !validateToken(token?.toString())) {
+    if(pathname.startsWith('/auth/login') && !(await validateToken(token?.toString()))) {
         return
     }
 
-    if(req.url.includes('/auth/login') && await validateToken(token?.toString())) {
+    if(req.url.includes('/auth/login') && (await validateToken(token?.toString()))) {
         return NextResponse.redirect(new URL('/home', req.url))
     }
     
-    if(!validateToken(token?.toString())) {
+    if(!(await validateToken(token?.toString()))) {
         return NextResponse.redirect(new URL('/auth/login', req.url))
     }
 }
--- src/util/validateToken.ts
+++ src/util/validateToken.ts
@@ -4,7 +4,6 @@ export default async function validateToken(token: string) {
     const secret = new TextEncoder().encode(process.env.NEXT_PUBLIC_JWT_SECRET)
     try {
         const { payload } = await jose.jwtVerify(token, secret)
-        console.log(payload)
         return true
     } catch(error) {
         console.log(error)
